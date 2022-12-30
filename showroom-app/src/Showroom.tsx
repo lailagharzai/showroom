7ce9d49543becfe813efca41480d133742d60c2c
Laila Gharzai <laila.gharzai@deadhappy.com>
2022-12-30 20:15:54 +0000

    Transferring index and css to app

--- showroom-app/src/Showroom.tsx
+++ showroom-app/src/Showroom.tsx
@@ -3,7 +3,153 @@ import "./Showroom.css";
 
 function Showroom() {
   return (
-   <div></div>
+    <div className="main">
+    <div className="cards">
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">JKHJHJKHKHKJ</p>
+          <p className="card__price2 text--medium">£2000</p>
+          <p className="card__description text--medium">This is a cool car</p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+            
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">kgjjhgjkhg</p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1473283147055-e39c51463929?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1482784160316-6eb046863ece?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1470104240373-bc1812eddc9f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1486870591958-9b9d0d1dda99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1534143046043-44af3469836b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image-container">
+          <img
+            src="https://images.unsplash.com/photo-1465056836041-7f43ac27dcb5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1200&q=80"
+          />
+        </div>
+        <div className="card__content">
+          <p className="card__title text--medium">
+            Here's the Title of an Awesome Course
+          </p>
+          <div className="card__info">
+            <p className="text--medium">30 Min</p>
+            <p className="card__price text--medium">Free</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
   );
 }
 
