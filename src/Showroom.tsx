995d0aa22b3194475c1f1b2fe29516e3a0ed07fd
Laila Gharzai <laila.gharzai@deadhappy.com>
2023-01-10 23:12:08 +0000

    cleaning up and adding car cards'

--- src/Showroom.tsx
+++ src/Showroom.tsx
@@ -13,30 +13,13 @@ const Main = styled.div`
 const Cards = styled.div`
   grid-column: 2 / span 12;
   display: grid;
-  grid-template-columns: repeat(12, minmax(auto, 60px));
+  grid-template-columns: repeat(12, 1fr);
   grid-gap: 40px;
   padding: 32px 0;
-  
-  @media only screen and (max-width: 500px) {
-    grid-column: 2 / span 6;
-    grid-template-columns: repeat(6, 1fr);
-    grid-gap: 20px;
-  }
 `;
 
 const Content = styled.div`
   padding-left: 20px;
-  grid-column: 2 / span 12;
-  padding-top: 60px;
-
-  @media only screen and (max-width: 500px) {
-    grid-column: 2 / span 6;
-    margin-left: 50px;
-  }
-
-  @media only screen and (max-width: 400px) {
-    padding-top: 0;
-  }
 `;
 
 function Showroom() {
