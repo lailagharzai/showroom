220c3515dd89f4c7f21c770abb34588fdb277ba2
Laila Gharzai <laila.gharzai@deadhappy.com>
2023-01-13 15:05:51 +0000

    index render CarsForSale

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CarsForSale from './components/pages/CarsForSale';
+import CarsForSale from './components/pages/CarsForSale/CarsForSale';
 
 
 const root = ReactDOM.createRoot(
